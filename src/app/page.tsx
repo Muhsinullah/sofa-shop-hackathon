2f20513578b0723a3c38f1dbb8e3c58f3f7d7917
Muhsinullah <muhsinkhanj786@gmail.com>
2024-12-09 00:58:23 +0500

    fixed
    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ProductShowcase from "./components/ProductShowcase";
 import TopPicks from "./components/TopPicks";
 import Blogs from "./components/Blogs";
 import Instagram from "./components/Instagram";
-import Footer from "./components/footer";
+// import Footer from "./components/footer";
 
 export default function Home() {
   return (
